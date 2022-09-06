c84964de309e56deffca3340eea3ab0e50c3d17b
goldfirework <jthallaug@gmail.com>
2022-09-06 13:49:00 +0200

    oransje

--- Oppgaer/Oppgave9.py
+++ Oppgaer/Oppgave9.py
@@ -1,4 +1,4 @@
-fargene = ["rød", "oransj", "gul", "grønn", "blå", "indigo", "fiolett"]
+fargene = ["rød", "oransje", "gul", "grønn", "blå", "indigo", "fiolett"]
 
 print("Den første fargen er: " + fargene[0])
 print("en fjerde fargen er: " + fargene[3])

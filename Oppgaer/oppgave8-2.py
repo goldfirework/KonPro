b66a2b4296d7206176c493124171ecbd599f1c78
goldfirework <jthallaug@gmail.com>
2022-09-14 14:48:18 +0200

    oppg 8-2

---
+++ Oppgaer/oppgave8-2.py
@@ -0,0 +1,25 @@
+print("\n\n\nHei og velkommen til quiz!\n")
+poeng = 0
+
+sporsmalsListe = [
+    "Hva heter Asias største øy? \n",
+    "I hvilket land ligger hovedkontoret til flyselskapet Ryanair? \n",
+    "I hvilken by ligger Chryslerbygningen? \n"
+    ]
+riktigSvar = [
+    "borneo",
+    "irland",
+    "new york"
+    ]
+
+x = 0
+while x < len(sporsmalsListe):
+    svar = input(sporsmalsListe[x])
+    if riktigSvar[x] == svar.lower():
+        print("\nRiktig!\n")
+        poeng += 1
+    else:
+        print("Feil!")
+    x += 1
+
+print("Du fikk " + poeng + " poeng!\n")

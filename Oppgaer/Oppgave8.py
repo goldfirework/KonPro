49c5e272f60f5c25842e54cba1d11b8e1f117803
goldfirework <jthallaug@gmail.com>
2022-09-14 14:24:57 +0200

    quest - question

--- Oppgaer/Oppgave8.py
+++ Oppgaer/Oppgave8.py
@@ -1,5 +1,5 @@
 poengsum = 0
-def quest(type, question, answer1, answer2, answer3, answer4, riktig, range, poeng):
+def question(type, question, answer1, answer2, answer3, answer4, riktig, range, poeng):
     #type 1 = tall spørsmål, kun et svar.
     #type 2 = multi svar spørsmål. 1-4 svar
     #type = velg 1 eller 2 : answer1 bruker av både type 1 og type 2 : answer2-4 brukes bare av type 2 sett til 0 på type 1.
@@ -63,7 +63,7 @@ def quest(type, question, answer1, answer2, answer3, answer4, riktig, range, poe
     else:
         print("bare velg mellom type 1 og 2")
 
-poengsum = poengsum + quest(1, "Hvor høy er verdens høyeste bygning?", 828, 0, 0, 0, 1, 10, 1)
-poengsum = poengsum + quest(2, "Hvor raskt løper et rovdyr?", 30, 50, 70, 80, 3, 0, 1)
-poengsum = poengsum + quest(2, "Hva er en velosiped", "Ord for fart", "Ord for akselerasjon", "En sykkel", 0, 3, 0, 1)
+poengsum = poengsum + question(1, "Hvor høy er verdens høyeste bygning?", 828, 0, 0, 0, 1, 10, 1)
+poengsum = poengsum + question(2, "Hvor raskt løper et rovdyr?", 30, 50, 70, 80, 3, 0, 1)
+poengsum = poengsum + question(2, "Hva er en velosiped", "Ord for fart", "Ord for akselerasjon", "En sykkel", 0, 3, 0, 1)
 print("Du fikk " + str(poengsum) + " poeng")
